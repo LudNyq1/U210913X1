e37f2fea0c88e74f5f90a012e586e97ee540dc66
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-13 12:40:57 +0200

    Skapat car klassen och meny.

--- ACME Cars/Program.cs
+++ ACME Cars/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACME_Cars
 {
-    class Program
+    class Program //GLÖM EJ ATT COMMITA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Cars> car = new List<Cars>(); //Detta ska du träna på att skriva!
+            bool a = true;
+                
+            while (a = true)
+            {
+                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                Console.WriteLine("Welcome to ACME Cars industry program Copyrighted est.1999");
+                Console.WriteLine("===========================================================");
+                Console.WriteLine("S\tShow all cars\nX\tAdd car\nE\tEdit car\nD\tRemove car\nX\tExit program\n");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "S":
+                            break;
+                    case "S":
+                        break;
+                    case "S":
+                        break;
+                    case "S":
+                        break;
+
+                }
+            }
         }
+    }
+
+    public class Cars //Color, Model, Make och Price i domänmodellen
+    {
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Price { get; set; }
     }
 }
